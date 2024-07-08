904f503d9f2a3d6a54f1e748fa9ee200c6afb3b5
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-07-08 23:45:55 +0300

    dev: about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,33 @@
+'use client';
+
+import {
+    useContext,
+} from 'react';
+
+import {
+    LanguageContext,
+} from '@/app/context';
+
 import PageDetail from '@/components/PageDetail';
 
 
 
 export default function About() {
+    const {
+        language,
+    } = useContext(LanguageContext);
+
     return (
         <PageDetail
-            name="about"
+            name={language === 'en' ? 'about' : 'despre'}
         >
             <p
                 className="text-center mb-8"
             >
-                atemporal film is a features and shorts production company
+                {language === 'en'
+                    ? 'atemporal film is a features and shorts production company'
+                    : 'atemporal film este o companie de producție de lungmetraje și scurtmetraje'
+                }
             </p>
         </PageDetail>
     );
