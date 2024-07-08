d35b36ba61c01abbc66dd7df7ff7a8f08b4811b6
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-07-08 17:35:40 +0300

    dev: contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,77 @@
+'use client';
+
+import {
+    useContext,
+} from 'react';
+
+import {
+    LanguageContext,
+} from '@/app/context';
+
 import PageDetail from '@/components/PageDetail';
 
 
 
 export default function Contact() {
+    const {
+        language,
+    } = useContext(LanguageContext);
+
     return (
         <PageDetail
             name="contact"
         >
-            <p
+            <div
                 className="text-center mb-8"
             >
-                for inquiries, please email <a href="mailto:contact@atemporalfilm.com">contact@atemporalfilm.com</a>
-            </p>
+                <p>
+                    ȘTEFAN AZAHARIOAIE
+                </p>
+
+                <p>
+                    {language === 'en' ? 'sound designer, editor' : 'design sunet, monteur'}
+                </p>
+
+                <p>
+                    <a href="tel:+40743343800">+40 743 343 800</a>
+                </p>
+
+                <p>
+                    <a href="mailto:stefan@atemporalfilm.com">stefan@atemporalfilm.com</a>
+                </p>
+            </div>
+
+            <div
+                className="text-center mb-8"
+            >
+                <p>
+                    DIANA SMEU
+                </p>
+
+                <p>
+                    COO
+                </p>
+
+                <p>
+                    <a href="tel:++40770789376">+40 770 789 376</a>
+                </p>
+
+                <p>
+                    <a href="mailto:stefan@atemporalfilm.com">diana@atemporalfilm.com</a>
+                </p>
+            </div>
+
+            <div
+                className="text-center mb-2"
+            >
+                Piatra-Neamț, Traian 6
+            </div>
+
+            <div
+                className="text-center mb-8"
+            >
+                București, Vasile Lascăr 23-25
+            </div>
         </PageDetail>
     );
 }
