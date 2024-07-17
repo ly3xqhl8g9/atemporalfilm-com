f5c3ee0513f8c0ce2f0eed5f091f9c2cc88c43d4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-07-17 14:06:54 +0300

    dev: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import type { Viewport } from 'next';
+
 import { Didact_Gothic } from 'next/font/google';
 
 import './globals.css';
@@ -13,8 +15,48 @@ const inter = Didact_Gothic({
 });
 
 export const metadata: Metadata = {
+    applicationName: 'atemporalfilm.com',
     title: 'atemporal film',
     description: 'post-production sound, editing services, and field recording services',
+    keywords: 'post-production sound, editing services, field recording services',
+    category: 'Business',
+    creator: 'atemporalfilm.com',
+    publisher: 'atemporalfilm.com',
+    classification: 'Business',
+    robots: 'index, follow',
+    referrer: 'origin',
+    metadataBase: new URL('https://atemporalfilm.com'),
+    alternates: {
+        canonical: 'https://atemporalfilm.com',
+    },
+    icons: {
+        other: [
+            {
+                rel: 'mask-icon',
+                url: '/safari-pinned-tab.svg',
+                color: '#000000',
+            },
+        ],
+    },
+    openGraph: {
+        type: 'website',
+        url: 'https://atemporalfilm.com',
+        title: 'atemporalfilm.com',
+        description: 'post-production sound, editing services, and field recording services',
+        siteName: 'atemporalfilm.com',
+        images: [
+            {
+                url: 'https://atemporalfilm.com/og.png',
+            },
+        ],
+    },
+    other: {
+        'msapplication-TileColor': '#000000',
+    },
+};
+
+export const viewport: Viewport = {
+    themeColor: '#000000',
 };
 
 export default function RootLayout({
